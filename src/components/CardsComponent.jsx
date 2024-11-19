9bc934b9ceefc36aa808442a81ae36c7b8fb3345
cmorales001 <cristiandmorales1a@gmail.com>
2024-11-19 11:02:55 -0500

    update CardComponent: code optimization

--- src/components/CardsComponent.jsx
+++ src/components/CardsComponent.jsx
@@ -6,39 +6,46 @@ const CardsComponent = ({ numCardsInput }) => {
 
   const [cards, setCards] = useState([]);
   const debounceValueNumCards = useDebounce(numCardsInput, 1000);
+  const [error, setError] = useState(null);
 
 
   useEffect(() => {
     const fetchCards = async () => {
+      setError(null);
+
       try {
         const response = await fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${debounceValueNumCards}`);
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+
         const data = await response.json();
-        console.log(data)
-
-
         setCards(data.cards);
       } catch (e) {
-        console.log(e);
+        setError( 'OCURRIO UN PROBLEMA CON EL SISTEMA, INTENTALO MÁS TARDE.');
+        setCards([]);
       }
     };
 
     debounceValueNumCards !== 0 ? fetchCards() : setCards([]);
-    console.log("use efect")
   }, [debounceValueNumCards]);
 
   return (
-    <>
-      {cards.map((card) => (
-        <img
-          src={card.image}
-          id={card.code}
-          alt={card.code}
-
-        />
-      )
-
+    <div>
+      {error ? (
+        <h1>{`ERROR: ${error}`}</h1>
+      ) : (
+        cards.map((card) => (
+          <img
+            key={card.code}
+            src={card.image}
+            alt={card.code}
+            id={card.code}
+          />
+        ))
       )}
-    </>
+    </div>
 
 
   );
